f65121214ec625c79f34042b31dfe94de78314e4
notmarkmiranda <notmarkmiranda@gmail.com>
2019-03-26 16:48:20 -0600

    add leagues migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_223959) do
+ActiveRecord::Schema.define(version: 2019_03_26_224338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
 
-  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "leagues", force: :cascade do |t|
+    t.string "name"
+    t.boolean "active", default: true
+    t.date "start_date"
+    t.date "end_date"
+    t.boolean "privated", default: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_leagues_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -38,4 +50,5 @@ ActiveRecord::Schema.define(version: 2019_03_26_223959) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "leagues", "users"
 end
