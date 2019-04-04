a284bc11ac7f5bfe66114aa94106b837d20b4d66
notmarkmiranda <notmarkmiranda@gmail.com>
2019-04-04 09:19:10 -0600

    add request specs

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -4,12 +4,7 @@ class LeaguesController < ApplicationController
   def show
     @league = League.find(params[:id]).decorate
     @teams = TeamDecorator.decorate_collection(Team.ordered_teams_by_league(@league.id))
-
-    # this will eventually live on the league model or in settings
     @range = [*0..13]
-
-    # TODO: make accomplishments after a certain Date
-    # also filter on the view based on when the ownership started
     @accomplishments = Accomplishment.filter_by_league(@league)
   end
 
--- spec/requests/ownerships_controller_request_spec.rb
+++ spec/requests/ownerships_controller_request_spec.rb
@@ -29,5 +29,79 @@ describe OwnershipsController, type: :request do
     end
   end
 
-  describe 'POST#create'
+  describe 'POST#create' do
+    subject { post league_ownerships_path(league), params: { ownership: ownership_params } }
+
+    let(:team) { create(:team) }
+    let(:ownership_params) { attributes_for(:ownership).merge(user: { email: owner_email }, team_id: team.id) }
+
+    before { sign_in(admin) }
+    describe 'for a new user' do
+      let(:owner_email) { 'test@example.com' }
+
+      it 'should create a new user and a new ownership' do
+        expect {
+          subject
+        }.to change(User, :count).by(1)
+        .and change(Ownership, :count).by(1)
+        .and change(Membership, :count).by(1)
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    describe 'for an existing user' do
+      let!(:user) { create(:user) }
+      let(:owner_email) { user.email }
+
+      it 'should only create a new ownership and not a new user' do
+        expect {
+          subject
+        }.to change(User, :count).by(0)
+        .and change(Ownership, :count).by(1)
+        .and change(Membership, :count).by(1)
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    describe 'for the existing member' do
+      let!(:user) { create(:membership, league: league).user }
+      let(:owner_email) { user.email }
+
+      it 'should only create a new ownership and not a new membership' do
+        expect {
+          subject
+        }.to change(User, :count).by(0)
+        .and change(Ownership, :count).by(1)
+        .and change(Membership, :count).by(0)
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    describe 'for logged in user' do
+      let(:owner_email) { admin.email }
+
+      it 'should create an ownership only' do
+        expect {
+          subject
+        }.to change(User, :count).by(0)
+        .and change(Ownership, :count).by(1)
+        .and change(Membership, :count).by(0)
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    describe 'for user with a team already' do
+      let(:new_team) { create(:team) }
+      let!(:user) { create(:ownership, team: new_team, league: league).user }
+      let(:owner_email) { user.email }
+
+      it 'does not create anything' do
+        expect {
+          subject
+        }.to change(User, :count).by(0)
+        .and change(Ownership, :count).by(0)
+        .and change(Membership, :count).by(0)
+      end
+    end
+  end
 end
