f016dec796798e6d18a55004de42a08f707aeb46
notmarkmiranda <notmarkmiranda@gmail.com>
2019-04-04 21:01:46 -0600

    add token to league and change after login path

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_01_231337) do
+ActiveRecord::Schema.define(version: 2019_04_05_023459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2019_04_01_231337) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
+    t.index ["token"], name: "index_leagues_on_token", unique: true
     t.index ["user_id"], name: "index_leagues_on_user_id"
   end
 
