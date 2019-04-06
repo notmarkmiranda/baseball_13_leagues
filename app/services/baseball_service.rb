dfb99c59d53ca48e20eebb4f37ee07417665d93f
notmarkmiranda <notmarkmiranda@gmail.com>
2019-04-05 23:21:21 -0600

    refactor import day

--- app/services/baseball_service.rb
+++ app/services/baseball_service.rb
@@ -7,8 +7,8 @@ class BaseballService
     @uri = URI(url)
   end
 
-  def self.go!(year:, month:, day:)
-    url = "http://gd2.mlb.com/components/game/mlb/year_#{year}/month_#{month}/day_#{day}/master_scoreboard.json"
+  def self.go!(date)
+    url = "http://gd2.mlb.com/components/game/mlb/year_#{date[:year]}/month_#{date[:month]}/day_#{date[:day]}/master_scoreboard.json"
     new(url).vamonos!
   end
 
--- spec/lib/import_day_spec.rb
+++ spec/lib/import_day_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 describe ImportDay do
   let(:json_file) { file_fixture('04012019_master_scoreboard.json').read }
+  let(:date) { Date.today }
 
-  subject { described_class.new(json_file).import! }
+  subject { described_class.lets_go!(date) }
 
-  before { allow(ImportGame).to receive(:import!) }
-  it 'should call the CreateGame class for games that are finished' do
-    subject
+  describe 'with a date object' do
+    before { allow(ImportGame).to receive(:import!) }
+    it 'should call the CreateGame class for games that are finished' do
+      expect(BaseballService).to receive(:go!).with(date).and_return(json_file)
+      subject
 
-    expect(ImportGame).to have_received(:import!).exactly(9).times
+      expect(ImportGame).to have_received(:import!).exactly(9).times
+    end
   end
 end
