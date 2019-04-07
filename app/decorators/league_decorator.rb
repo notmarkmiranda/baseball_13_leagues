bc8392bd5cafa9b90d0a26a3be53d7a9dc053f0f
notmarkmiranda <notmarkmiranda@gmail.com>
2019-04-07 10:24:57 -0600

    change in progress text on league show

--- app/decorators/league_decorator.rb
+++ app/decorators/league_decorator.rb
@@ -5,8 +5,12 @@ class LeagueDecorator < ApplicationDecorator
     object.active? ? 'Active League' : 'Done'
   end
 
-  def full_start_date
-    start_date.strftime("%B %-e, %Y")
+  def full_date(timeframe)
+    object.send(timeframe).strftime("%B %-e, %Y")
+  end
+
+  def in_progress_text
+    object.end_date ? full_date('end_date') : ' - In Progress'
   end
 
   def memberships_translator
