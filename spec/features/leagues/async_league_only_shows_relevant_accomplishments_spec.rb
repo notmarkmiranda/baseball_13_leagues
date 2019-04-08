2239a561ea03fb24bf0a8e1dacde9160f99c219f
notmarkmiranda <notmarkmiranda@gmail.com>
2019-04-08 07:04:42 -0600

    Change ImportDay to ImportGame

---
+++ spec/features/leagues/async_league_only_shows_relevant_accomplishments_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'Async league only shows relevant accomplishments' do
+  let(:league_start) { Date.new(2015, 5, 9) }
+  let(:late_start) { Date.new(2015, 5, 11) }
+
+  let(:league) { create(:league, async_starts: true, start_date: league_start) }
+  let(:ownership) { create(:ownership, league: league, start_date: late_start) }
+  let(:team) { ownership.team }
+
+  before do
+    @acc = create(:accomplishment, team: team, number: 1, date: league_start)
+  end
+
+  it 'should not show the accomplishment on the page' do
+    visit league_path(league)
+
+    expect(page).to have_content(league.name)
+    expect(page).not_to have_selector(:css, "i.fa.fa-check")
+  end
+end
